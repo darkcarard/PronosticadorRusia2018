098314c49ff0b1b7af9abb12e6add43f10e3c475
Darkcarard <carlosardilab@gmail.com>
2018-02-24 13:53:24 -0500

    Limpieza de exports

--- App/PronosticadorRusia2018/PronosticadorRusia2018-war/src/co/pronosticador/view/mb/seguridad/LoginBean.java
+++ App/PronosticadorRusia2018/PronosticadorRusia2018-war/src/co/pronosticador/view/mb/seguridad/LoginBean.java
@@ -6,10 +6,7 @@ import co.pronosticador.view.util.JSFUtils;
 import java.io.Serializable;
 import javax.ejb.EJB;
 import javax.enterprise.context.SessionScoped;
-import javax.faces.application.FacesMessage;
-import javax.faces.context.FacesContext;
 import javax.inject.Named;
-import javax.servlet.http.HttpSession;
 
 @Named(value = "loginBean")
 @SessionScoped
